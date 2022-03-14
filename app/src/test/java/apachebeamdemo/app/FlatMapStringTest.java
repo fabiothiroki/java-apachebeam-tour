8303469c9d9b2e1b7d3f2b413bf4695fa788c855
Fabio Hiroki <fabio.hiroki@quintoandar.com.br>
2022-03-14 08:25:38 +0100

    Add group chain example (#7)
    Co-authored-by: Fabio Hiroki <fabio.hiroki@mollie.com>

--- app/src/test/java/apachebeamdemo/app/FlatMapStringTest.java
+++ app/src/test/java/apachebeamdemo/app/FlatMapStringTest.java
@@ -2,8 +2,10 @@ package apachebeamdemo.app;
 
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.FlatMapElements;
+import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.TypeDescriptors;
 import org.junit.Rule;
@@ -15,7 +17,7 @@ import java.util.List;
 public class FlatMapStringTest {
 
     static final String[] WORDS_ARRAY = new String[] {
-            "hi bob", "hello alice"};
+            "hi bob", "hello alice", "hi sue"};
 
     static final List<String> WORDS = Arrays.asList(WORDS_ARRAY);
 
@@ -32,7 +34,29 @@ public class FlatMapStringTest {
         );
 
         PAssert.that(output)
-                .containsInAnyOrder("hi", "bob", "hello", "alice");
+                .containsInAnyOrder("hi", "bob", "hello", "alice", "hi", "sue");
+
+        pipeline.run();
+    }
+
+    @Test
+    public void testCountEachWord() {
+        PCollection<String> input = pipeline.apply(Create.of(WORDS));
+
+        PCollection<KV<String, Long>> output = input
+                .apply(
+                    FlatMapElements.into(TypeDescriptors.strings())
+                        .via((String line) -> Arrays.asList(line.split(" ")))
+                )
+                .apply(Count.<String>perElement());
+
+        PAssert.that(output)
+                .containsInAnyOrder(
+                        KV.of("hi", 2L),
+                        KV.of("hello", 1L),
+                        KV.of("alice", 1L),
+                        KV.of("sue", 1L),
+                        KV.of("bob", 1L));
 
         pipeline.run();
     }
